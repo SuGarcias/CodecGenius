ed8c51be97bdd5b6c06d81903451e699340ac6d8
Pol Sedo <100866973+polsedo@users.noreply.github.com>
2023-05-29 23:29:24 +0200

    ruta_absoluta

--- cgenius.py
+++ cgenius.py
@@ -5,6 +5,8 @@ import socket
 import shutil
 import speech_recognition as sr
 
+FFMPEG_PATH = "C:/ffmpeg.exe"
+
 def delete_file(file_path):
     try:
         os.remove(file_path)
@@ -23,20 +25,16 @@ def zip_file(input_file):
 
 def unzip_file(input_file):
     shutil.unpack_archive(input_file)
-#############################################################
-def generate_and_embed_subtitles(video_file, output_file):
-    # Extraer el audio del video
-    audio_file = "temp_audio.wav"
-    ffmpeg.input(video_file).output(audio_file, format="wav").run()
 
-    # Generar los subtítulos
+def generate_and_embed_subtitles(video_file, output_file):
+    audio_file = "temp_audio.wav"
+    ffmpeg.input(video_file, executable=FFMPEG_PATH).output(audio_file, format="wav").run()
+
     subtitles_file = "subtitles.srt"
     generate_subtitles(audio_file, subtitles_file)
 
-    # Incrustar los subtítulos en el video
-    ffmpeg.input(video_file).output(output_file, vf="subtitles='" + subtitles_file + "'").run()
+    ffmpeg.input(video_file, executable=FFMPEG_PATH).output(output_file, vf="subtitles='" + subtitles_file + "'").run()
 
-    # Eliminar los archivos temporales
     os.remove(audio_file)
     os.remove(subtitles_file)
 
@@ -47,7 +45,7 @@ def generate_subtitles(audio_file, output_file):
         audio = recognizer.record(source)
         text = recognizer.recognize_google(audio, show_all=False)
 
-    duration = ffmpeg.probe(audio_file)['format']['duration']
+    duration = ffmpeg.probe(audio_file, executable=FFMPEG_PATH)['format']['duration']
     duration = float(duration)
 
     subtitle_text = "1\n00:00:00,000 --> {duration},000\n{text}".format(duration=to_srt_time(duration), text=text)
@@ -60,49 +58,44 @@ def to_srt_time(seconds):
     m, s = divmod(int(seconds), 60)
     h, m = divmod(m, 60)
     return "{:02d}:{:02d}:{:02d},{:03d}".format(h, m, s, milliseconds)
-####################################################################
+
 def send_file(input_file, remote_IP, port):
     port = int(port)
     base_name = os.path.splitext(input_file)[0]
     zip_file(input_file)
-    input_zip = base_name +'.zip'
-    # Establecer conexión con el dispositivo de destino
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET --> IPv4;   SOCK_STREAM --> Socket TCP
+    input_zip = base_name + '.zip'
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((remote_IP, port))
     print('Conectado!')
-    # Abrir el archivo de video y leer los datos en bloques
+
     with open(input_zip, 'rb') as archivo:
         while True:
-            datos = archivo.read(1024)  # Leer 1KB de datos del video
+            datos = archivo.read(1024)
             if not datos:
                 break
-            sock.sendall(datos)  # Enviar los datos al dispositivo de destino
+            sock.sendall(datos)
 
-    # Cerrar la conexión
     sock.close()
     delete_file(input_zip)
 
 def recive_file(port):
     port = int(port)
-    # Crear un socket y enlazarlo a una dirección y puerto locales
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('0.0.0.0', port)) # 0.0.0.0 --> Cualquier dirección
-    sock.listen(1)  # Esperar 1 sola conexion entrante
+    sock.bind(('0.0.0.0', port))
+    sock.listen(1)
 
-    # Aceptar la conexión entrante
     print("Esperando conexión entrante...")
     conexion, direccion = sock.accept()
     print("Conexión establecida desde:", direccion)
 
-    # Abrir un archivo para escribir los datos recibidos
     with open('archivo_socket.zip', 'wb') as archivo:
         while True:
-            datos = conexion.recv(1024)  # Recibir 1KB de datos
+            datos = conexion.recv(1024)
             if not datos:
                 break
-            archivo.write(datos)  # Escribir los datos en el archivo
+            archivo.write(datos)
 
-    # Cerrar la conexión
     conexion.close()
     sock.close()
     print('conexion cerrada')
@@ -110,44 +103,20 @@ def recive_file(port):
     print('descomprimido')
     delete_file('archivo_socket.zip')
 
-
 def convert_video(input_file, output_file):
-    ffmpeg.input(input_file).output(output_file).run()
+    ffmpeg.input(input_file, executable=FFMPEG_PATH).output(output_file).run()
 
 def resize_video(input_file, output_file, width, height):
-    ffmpeg.input(input_file).output(output_file, vf=f'scale={width}:{height}').run()
+    ffmpeg.input(input_file, executable=FFMPEG_PATH).output(output_file, vf=f'scale={width}:{height}').run()
 
 def change_framerate(input_file, output_file, framerate):
-    ffmpeg.input(input_file).output(output_file, r=framerate).run()
+    ffmpeg.input(input_file, executable=FFMPEG_PATH).output(output_file, r=framerate).run()
 
 def adjust_quality(input_file, output_file, crf):
-    ffmpeg.input(input_file).output(output_file, crf=crf).run()
+    ffmpeg.input(input_file, executable=FFMPEG_PATH).output(output_file, crf=crf).run()
 
 def extract_video_segment(input_file, output_file, start_time, duration):
-    ffmpeg.input(input_file, ss=start_time, t=duration).output(output_file).run()
-
-############## FUNCIONS NO IMPLEMENTADES AL DOCOPT
-def extract_audio(input_file, output_file):
-    """
-    Extrae el audio de un archivo de video.
-
-    input_file: Ruta del archivo de entrada.
-    output_file: Ruta del archivo de salida de audio.
-    """
-    probe = ffmpeg.probe(input_file)
-    audio_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'audio'), None)
-
-    if audio_stream:
-        codec_name = audio_stream['codec_name']
-        output_file = f"{output_file}.{codec_name}"
-
-        ffmpeg.input(input_file).output(output_file, acodec='copy').run()
-    else:
-        print("No se encontró ningún flujo de audio en el archivo de video.")
-    
-##############
-
-
+    ffmpeg.input(input_file, executable=FFMPEG_PATH, ss=start_time, t=duration).output(output_file).run()
 
 USAGE = '''
  
