4912a5e1394e970f8b9850ee59233bcfb0d8d536
ORIOL GARCIA VILA <oriol.garcia.vila@estudiantat.upc.edu>
2023-05-29 23:46:59 +0200

    path

--- cgenius.py
+++ cgenius.py
@@ -5,8 +5,6 @@ import socket
 import shutil
 import speech_recognition as sr
 
-FFMPEG_PATH = "C:/ffmpeg.exe"
-
 def delete_file(file_path):
     try:
         os.remove(file_path)
@@ -25,16 +23,20 @@ def zip_file(input_file):
 
 def unzip_file(input_file):
     shutil.unpack_archive(input_file)
-
+#############################################################
 def generate_and_embed_subtitles(video_file, output_file):
+    # Extraer el audio del video
     audio_file = "temp_audio.wav"
-    ffmpeg.input(video_file, executable=FFMPEG_PATH).output(audio_file, format="wav").run()
+    ffmpeg.input(video_file).output(audio_file, format="wav").run()
 
+    # Generar los subtítulos
     subtitles_file = "subtitles.srt"
     generate_subtitles(audio_file, subtitles_file)
 
-    ffmpeg.input(video_file, executable=FFMPEG_PATH).output(output_file, vf="subtitles='" + subtitles_file + "'").run()
+    # Incrustar los subtítulos en el video
+    ffmpeg.input(video_file).output(output_file, vf="subtitles='" + subtitles_file + "'").run()
 
+    # Eliminar los archivos temporales
     os.remove(audio_file)
     os.remove(subtitles_file)
 
@@ -45,7 +47,7 @@ def generate_subtitles(audio_file, output_file):
         audio = recognizer.record(source)
         text = recognizer.recognize_google(audio, show_all=False)
 
-    duration = ffmpeg.probe(audio_file, executable=FFMPEG_PATH)['format']['duration']
+    duration = ffmpeg.probe(audio_file)['format']['duration']
     duration = float(duration)
 
     subtitle_text = "1\n00:00:00,000 --> {duration},000\n{text}".format(duration=to_srt_time(duration), text=text)
@@ -58,44 +60,49 @@ def to_srt_time(seconds):
     m, s = divmod(int(seconds), 60)
     h, m = divmod(m, 60)
     return "{:02d}:{:02d}:{:02d},{:03d}".format(h, m, s, milliseconds)
-
+####################################################################
 def send_file(input_file, remote_IP, port):
     port = int(port)
     base_name = os.path.splitext(input_file)[0]
     zip_file(input_file)
-    input_zip = base_name + '.zip'
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    input_zip = base_name +'.zip'
+    # Establecer conexión con el dispositivo de destino
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET --> IPv4;   SOCK_STREAM --> Socket TCP
     sock.connect((remote_IP, port))
     print('Conectado!')
-
+    # Abrir el archivo de video y leer los datos en bloques
     with open(input_zip, 'rb') as archivo:
         while True:
-            datos = archivo.read(1024)
+            datos = archivo.read(1024)  # Leer 1KB de datos del video
             if not datos:
                 break
-            sock.sendall(datos)
+            sock.sendall(datos)  # Enviar los datos al dispositivo de destino
 
+    # Cerrar la conexión
     sock.close()
     delete_file(input_zip)
 
 def recive_file(port):
     port = int(port)
+    # Crear un socket y enlazarlo a una dirección y puerto locales
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('0.0.0.0', port))
-    sock.listen(1)
+    sock.bind(('0.0.0.0', port)) # 0.0.0.0 --> Cualquier dirección
+    sock.listen(1)  # Esperar 1 sola conexion entrante
 
+    # Aceptar la conexión entrante
     print("Esperando conexión entrante...")
     conexion, direccion = sock.accept()
     print("Conexión establecida desde:", direccion)
 
+    # Abrir un archivo para escribir los datos recibidos
     with open('archivo_socket.zip', 'wb') as archivo:
         while True:
-            datos = conexion.recv(1024)
+            datos = conexion.recv(1024)  # Recibir 1KB de datos
             if not datos:
                 break
-            archivo.write(datos)
+            archivo.write(datos)  # Escribir los datos en el archivo
 
+    # Cerrar la conexión
     conexion.close()
     sock.close()
     print('conexion cerrada')
@@ -103,23 +110,27 @@ def recive_file(port):
     print('descomprimido')
     delete_file('archivo_socket.zip')
 
+
 def convert_video(input_file, output_file):
-    ffmpeg.input(input_file, executable=FFMPEG_PATH).output(output_file).run()
+    ffmpeg.input(input_file).output(output_file).run()
 
 def resize_video(input_file, output_file, width, height):
-    ffmpeg.input(input_file, executable=FFMPEG_PATH).output(output_file, vf=f'scale={width}:{height}').run()
+    ffmpeg.input(input_file).output(output_file, vf=f'scale={width}:{height}').run()
 
 def change_framerate(input_file, output_file, framerate):
-    ffmpeg.input(input_file, executable=FFMPEG_PATH).output(output_file, r=framerate).run()
+    ffmpeg.input(input_file).output(output_file, r=framerate).run()
 
 def adjust_quality(input_file, output_file, crf):
-    ffmpeg.input(input_file, executable=FFMPEG_PATH).output(output_file, crf=crf).run()
+    ffmpeg.input(input_file).output(output_file, crf=crf).run()
 
 def extract_video_segment(input_file, output_file, start_time, duration):
-    ffmpeg.input(input_file, executable=FFMPEG_PATH, ss=start_time, t=duration).output(output_file).run()
+    ffmpeg.input(input_file, ss=start_time, t=duration).output(output_file).run()
 
 def extract_audio(input_file, output_file):
-    ffmpeg.input(input_file, executable=FFMPEG_PATH).output(output_file, format="wav").run()
+    ffmpeg.input(input_file).output(output_file, format="wav").run()
+
+
+
 
 USAGE = '''
  
@@ -138,18 +149,18 @@ Oriol Garcia Vila | Pol Sedo i Mota
 ╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╚════╝
                                                 NETWORK MULTIMEDIA CODING TOOLS                                                                                                                                                                    
 Usage:
-  cgenius.py convert <input_file> <output_file>
-  cgenius.py resize <input_file> <output_file> --width <width> --height <height>
-  cgenius.py framerate <input_file> <output_file> --framerate <framerate>
-  cgenius.py quality <input_file> <output_file> --crf <crf>
-  cgenius.py extract <input_file> <output_file> --start_time <start_time> --duration <duration>
-  cgenius.py audio <input_file> <output_file>
-  cgenius.py send <input_file> <remote_IP> <port>
-  cgenius.py recive <port>
-  cgenius.py zip <input_file>
-  cgenius.py unzip <input_file>
-  cgenius.py delete <input_file>
-  cgenius.py subtitles <input_file> <output_file>
+  programa.py convert <input_file> <output_file>
+  programa.py resize <input_file> <output_file> --width <width> --height <height>
+  programa.py framerate <input_file> <output_file> --framerate <framerate>
+  programa.py quality <input_file> <output_file> --crf <crf>
+  programa.py extract <input_file> <output_file> --start_time <start_time> --duration <duration>
+  programa.py audio <input_file> <output_file>
+  programa.py send <input_file> <remote_IP> <port>
+  programa.py recive <port>
+  programa.py zip <input_file>
+  programa.py unzip <input_file>
+  programa.py delete <input_file>
+  programa.py subtitles <input_file> <output_file>
 
 
   
@@ -197,4 +208,5 @@ def main():
 
 
 if __name__ == "__main__":
+    os.environ['FFMPEG_PATH'] = 'C:/ffmpeg.exe'
     main()
